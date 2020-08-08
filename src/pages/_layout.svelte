577c8311faca5b565a4105f9eebaad1be7479a6c
Brian Ketelsen <mail@bjk.fyi>
2020-08-08 14:13:23 -0400

    first post

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { metatags, page } from "@sveltech/routify";
+  import Navigation from "./_navigation.svelte";
+  import SrcOnGithub from "../components/SrcOnGithub.svelte";
+
+  $: metatags.title = `BK : ${$page.title}`;
+  metatags.description = "Description coming soon...";
+</script>
+
+<div class="container">
+  <Navigation />
+  <slot />
+</div>
+
+<SrcOnGithub />
